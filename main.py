776da2413674902da9d5d7a48e2a5ca017577942
Your Name <you@example.com>
2026-06-14 13:44:57 +0200

    checking if we are dividing by zero, calc completed

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 import sys
-
-
-
 
 first_number = int(sys.argv[1])
 second_number = int(sys.argv[3])
@@ -25,5 +12,8 @@ if sys.argv[2] == "-":
     print(first_number - second_number)
 if sys.argv[2] == "*":
     print(first_number * second_number)
-if sys.argv[2] == "/":
+if second_number == 0:
+    print("We cant divide by zero")
+else:
     print(first_number / second_number)
+
