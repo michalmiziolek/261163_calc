a211c5fe80699ff67d65a247a70448cb8f086de8
Your Name <you@example.com>
2026-06-14 13:34:19 +0200

    comment while calculating added

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ import sys
 first_number = int(sys.argv[1])
 second_number = int(sys.argv[3])
 
-
+print("First number:", first_number)
+print("Second number:", second_number)
 if sys.argv[2] == "+":
     print(first_number + second_number)
 if sys.argv[2] == "-":
@@ -26,4 +27,3 @@ if sys.argv[2] == "*":
     print(first_number * second_number)
 if sys.argv[2] == "/":
     print(first_number / second_number)
-
