57d064aca04de4f82a34a6247d25eecae550156d
Michal45 <mymail@example.com>
2026-06-21 00:18:36 +0200

    dividing improved

--- simple_calc.py
+++ simple_calc.py
@@ -6,14 +6,18 @@ second_number = int(sys.argv[3])
 
 print("First number:", first_number)
 print("Second number:", second_number)
+
 if sys.argv[2] == "+":
     print(first_number + second_number)
+
 if sys.argv[2] == "-":
     print(first_number - second_number)
+
 if sys.argv[2] == "*":
     print(first_number * second_number)
-if second_number == 0:
-    print("We cant divide by zero")
-else:
-    print(first_number / second_number)
 
+if sys.argv[2] == "/":
+    if second_number == 0:
+        print("We cant divide by zero")
+    else:
+        print(first_number / second_number)
